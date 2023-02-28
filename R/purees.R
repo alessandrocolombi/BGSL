81f52a1b9f69a4e58609e58dcf8571ca99624d0e
Alessandro Colombi <colombi.alessandro@hotmail.it>
2023-02-28 16:00:33 +0100

    forgot to commit data documentation

---
+++ R/purees.R
@@ -0,0 +1,11 @@
+#' Absorbance mid-IR spectra of 351 strawberry fruit purées.
+#'
+#' This dataset contains the absorbance spectra of strawberry fruit purées, evaluated
+#' in 235 different wavelenghts.
+#'
+#' @usage data("purees")
+#'
+#' @format A list that contains two elements. \strong{wavelengths} contains the above-mentioned
+#' vector of wavelengths, while \strong{data} is a data frame with 351 rows and 235 variables:
+#' @source \url{https://csr.quadram.ac.uk/example-datasets-for-download/}
+"purees"
